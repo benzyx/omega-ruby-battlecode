5ddf4e3dedd1ef4a4ba672bfb954bb5e3222d0e1
Jacob Jackson <jacobbfjackson@gmail.com>
2017-01-12 16:52:18 -0500

    Basic movement + shooting

--- src/examplefuncsplayer/RobotPlayer.java
+++ src/examplefuncsplayer/RobotPlayer.java
@@ -6,10 +6,21 @@ public strictfp class RobotPlayer {
 	public static final int CHANNEL_NUMBER_OF_GARDENERS = 1;
 	public static final int CHANNEL_NUMBER_OF_SOLDIERS = 2;
 	public static final int CHANNEL_NUMBER_OF_LUMBERJACKS = 3;
+	public static final int CHANNEL_NUMBER_OF_SCOUTS = 4;
+	public static final int RALLY_POINT = 10;
 	
     static RobotController rc;
     static int myID;
     static boolean noGardenerYet = true;
+    static MapLocation destination = null;
+//    static Direction prevDirection = randomDirection();
+    static RobotInfo[] nearbyEnemies;
+    static RobotInfo[] nearbyFriends;
+    static BulletInfo[] nearbyBullets;
+    static TreeInfo[] nearbyTrees;
+    static MapLocation repeller = null;
+    static MapLocation[] history = new MapLocation[10];
+    
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
@@ -26,167 +37,246 @@ public strictfp class RobotPlayer {
         // You can add the missing ones or rewrite this into your own control structure.
         myID = rc.readBroadcast(myNumberOfChannel());
         rc.broadcast(myNumberOfChannel(), myID + 1);
-        switch (rc.getType()) {
-            case ARCHON:
-                runArchon();
-                break;
-            case GARDENER:
-                runGardener();
-                break;
-            case SOLDIER:
-                runSoldier();
-                break;
-            case LUMBERJACK:
-                runLumberjack();
-                break;
+        if (rc.getType() == RobotType.ARCHON)
+        {
+        	writePoint(RALLY_POINT, rc.getInitialArchonLocations(rc.getTeam().opponent())[0]);
+        }
+        destination = readPoint(RALLY_POINT);
+        while (true)
+        {
+        	try {
+        		int round = rc.getRoundNum();
+        		        		
+            	onRoundBegin();
+                if (nearbyEnemies.length > 0)
+                {
+                	writePoint(RALLY_POINT, nearbyEnemies[0].getLocation());
+                }
+                if (rc.getLocation().distanceTo(destination) < 10 || !rc.onTheMap(rc.getLocation().add(rc.getLocation().directionTo(destination), 3)))
+                {
+                	destination = rc.getLocation().add(randomDirection(), 100);
+                }
+            	switch (rc.getType())
+            	{
+            	case GARDENER:
+            		gardenerSpecificLogic();
+            		break;
+            	case ARCHON:
+            		archonSpecificLogic();
+            		break;
+            	}
+            	if (rc.getType() == RobotType.SCOUT || rc.getType() == RobotType.SOLDIER)
+            	{
+	            	MapLocation loc = selectOptimalMove();
+	            	if (rc.canMove(loc))
+	            	{
+	            		rc.move(loc);
+	            	}
+	            	long bestVal = 0;
+	            	Direction dir = null;
+	            	for (RobotInfo info : nearbyEnemies)
+	            	{
+	            		if (rc.getLocation().distanceTo(info.getLocation()) < 6)
+	            		{
+	            			long val = (long) getIdealDistanceMultiplier(info.getType());
+	            			if (dir == null || val > bestVal)
+	            			{
+	            				bestVal = val;
+	            				dir = rc.getLocation().directionTo(info.getLocation());	            				
+	            			}
+	            		}
+	            	}
+	            	if (rc.canFireSingleShot() && dir != null)
+	            	{
+	            		rc.fireSingleShot(dir);
+	            	}
+            	}
+            	
+            	onRoundEnd();
+            	
+            	if (round != rc.getRoundNum() || Clock.getBytecodesLeft() < 200)
+            	{
+            		throw new RuntimeException("Time limit exceeded!");
+            	}
+            	
+        		Clock.yield();
+        	}
+        	catch (Exception e)
+        	{
+        		System.out.println("Exception in robot loop");
+        		e.printStackTrace();
+        	}
         }
 	}
-
-    static void runArchon() throws GameActionException {
-        System.out.println("I'm an archon!");
-
-        // The code you want your robot to perform every round should be in this loop
-        while (true) {
-
-            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
-            try {
-
-                // Generate a random direction
-                Direction dir = randomDirection();
-
-                // Randomly attempt to build a gardener in this direction
-                if (rc.canHireGardener(dir) && myID == 0 && noGardenerYet) {
-                    rc.hireGardener(dir);
-                    noGardenerYet = false;
-                }
-
-                // Move randomly
-                // tryMove(randomDirection());
-
-                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-                Clock.yield();
-
-            } catch (Exception e) {
-                System.out.println("Archon Exception");
-                e.printStackTrace();
-            }
-        }
+    
+    public static void archonSpecificLogic() throws GameActionException
+    {
+    	if (myID == 0)
+    	{
+    		boolean seesGardener = false;
+	    	for (RobotInfo robot : nearbyFriends)
+	    	{
+	    		if (robot.getType() == RobotType.GARDENER)
+	    		{
+	    			seesGardener = true;
+	    			break;
+	    		}
+	    	}
+	    	if (!seesGardener)
+	    	{
+	        	attemptBuild(10, RobotType.GARDENER);    		
+	    	}
+    	}
     }
-
-	static void runGardener() throws GameActionException {
-        System.out.println("I'm a gardener!");
-
-        // The code you want your robot to perform every round should be in this loop
-        while (true) {
-
-            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
-            try {
-
-                // Generate a random direction
-                Direction dir = randomDirection();
-
-                if (rc.canBuildRobot(RobotType.LUMBERJACK, dir) && rc.isBuildReady()) {
-                    rc.buildRobot(RobotType.LUMBERJACK, dir);
-                }
-
-                // Move randomly
-                tryMove(randomDirection());
-
-                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-                Clock.yield();
-
-            } catch (Exception e) {
-                System.out.println("Gardener Exception");
-                e.printStackTrace();
-            }
-        }
+    
+    public static void attemptBuild(int iter, RobotType type) throws GameActionException
+    {
+    	for (int i = 0; i < iter; i++)
+    	{
+    		Direction dir = randomDirection();
+    		if (rc.canBuildRobot(type, dir))
+    		{
+    			rc.buildRobot(type, dir);
+    		}
+    	}
     }
-
-    static void runSoldier() throws GameActionException {
-        System.out.println("I'm an soldier!");
-        Team enemy = rc.getTeam().opponent();
-
-        // The code you want your robot to perform every round should be in this loop
-        while (true) {
-
-            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
-            try {
-                MapLocation myLocation = rc.getLocation();
-
-                // See if there are any nearby enemy robots
-                RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
-
-                // If there are some...
-                if (robots.length > 0) {
-                    // And we have enough bullets, and haven't attacked yet this turn...
-                    if (rc.canFireSingleShot()) {
-                        // ...Then fire a bullet in the direction of the enemy.
-                        rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
-                    }
-                }
-
-                // Move randomly
-                tryMove(randomDirection());
-
-                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-                Clock.yield();
-
-            } catch (Exception e) {
-                System.out.println("Soldier Exception");
-                e.printStackTrace();
-            }
-        }
+    
+    public static void gardenerSpecificLogic() throws GameActionException
+    {
+    	attemptBuild(10, RobotType.SOLDIER);
     }
-
-    static void runLumberjack() throws GameActionException {
-        System.out.println("I'm a lumberjack!");
-        Team enemy = rc.getTeam().opponent();
-
-        // The code you want your robot to perform every round should be in this loop
-        while (true) {
-
-            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
-            try {
-
-                // See if there are any enemy robots within striking range (distance 1 from lumberjack's radius)
-                RobotInfo[] robots = rc.senseNearbyRobots(RobotType.LUMBERJACK.bodyRadius+GameConstants.LUMBERJACK_STRIKE_RADIUS, enemy);
-
-                if(robots.length > 0 && !rc.hasAttacked()) {
-                    // Use strike() to hit all nearby robots!
-                    rc.strike();
-                } else {
-                    // No close robots, so search for robots within sight radius
-                    robots = rc.senseNearbyRobots(-1,enemy);
-
-                    // If there is a robot, move towards it
-                    if(robots.length > 0) {
-                        MapLocation myLocation = rc.getLocation();
-                        MapLocation enemyLocation = robots[0].getLocation();
-                        Direction toEnemy = myLocation.directionTo(enemyLocation);
-
-                        tryMove(toEnemy);
-                    } else {
-                        // Move Randomly
-                        tryMove(randomDirection());
-                    }
-                }
-
-                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-                Clock.yield();
-
-            } catch (Exception e) {
-                System.out.println("Lumberjack Exception");
-                e.printStackTrace();
-            }
-        }
+    
+    public static long badness(MapLocation loc)
+    {
+    	long ret = (long) (destination.distanceTo(loc) * 1000);
+    	
+    	ret -= (long) (loc.distanceTo(repeller) * 700);
+    	
+    	for (RobotInfo info : nearbyEnemies)
+    	{
+			float d = info.getLocation().distanceTo(loc);
+			float ideal = getIdealDistance(info.getType());
+			if (ideal < 0)
+				continue;
+			d -= ideal;
+			d *= d;
+			ret += (long) (d * getIdealDistanceMultiplier(info.getType()));
+    	}
+    	
+    	return ret;
     }
-
+    
+    public static float getIdealDistanceMultiplier(RobotType t)
+    {
+    	switch (t) {
+    	case LUMBERJACK:
+    		return 100000;
+    	case GARDENER:
+    		return 1000;
+    	case ARCHON:
+    		return 200;
+    	default:
+    		return 0;
+    	}
+    }
+    
+    public static float getIdealDistance(RobotType t)
+    {
+    	switch (t) {
+    	case LUMBERJACK:
+    		return 5; // 1.5 (lumberjack stride) + 1 radius (them) + 1 radius (us) + 1 (lumberjack attack range) + 0.5 (safety first kids)
+    	case GARDENER:
+    		return 2.1f;
+    	case ARCHON:
+    		return 3.1f;
+    	default:
+    		return 0;
+    	}
+    }
+    
+    public static MapLocation selectOptimalMove()
+    {
+    	if (rc.getType() != RobotType.SCOUT && rc.getType() != RobotType.SOLDIER)
+    		throw new RuntimeException();
+    	MapLocation best = null;
+    	long bestVal = 0;
+    	int iterations = 0;
+    	for (RobotInfo info : nearbyEnemies)
+    	{
+			MapLocation them = info.getLocation();
+			MapLocation us = rc.getLocation();
+			float d = them.distanceTo(us);
+			if (d < 10)
+			{
+				MapLocation cand = them.add(them.directionTo(us), getIdealDistance(info.getType()));
+				if (rc.canMove(cand))
+				{
+					long b = badness(cand);
+    				if (best == null || b < bestVal)
+    				{
+    					best = cand;
+    					bestVal = b;
+    				}
+				}
+			}
+    	}
+    	while (Clock.getBytecodesLeft() > 1000)
+    	{
+			MapLocation cand = rc.getLocation().add(randomDirection());
+			if (rc.canMove(cand))
+			{
+				long b = badness(cand);
+				if (best == null || b < bestVal)
+				{
+					best = cand;
+					bestVal = b;
+				}
+			}
+    		++iterations;
+    	}
+    	System.out.println(iterations + " iterations");
+    	if (best != null)
+    		return best;
+    	else
+    		return rc.getLocation();
+    	
+    }
+    
+    public static void onRoundEnd()
+    {
+    	
+    }
+    
+    public static void onRoundBegin()
+    {
+    	MapLocation old = history[rc.getRoundNum() % history.length];
+    	if (old != null && rc.getLocation().distanceTo(old) < 5)
+    	{
+    		repeller = old;
+    	}
+		history[rc.getRoundNum() % history.length] = rc.getLocation();
+    	nearbyFriends = rc.senseNearbyRobots(100, rc.getTeam());
+    	nearbyEnemies = rc.senseNearbyRobots(100, rc.getTeam().opponent());
+    	nearbyBullets = rc.senseNearbyBullets();
+    	nearbyTrees = rc.senseNearbyTrees();
+    }
+    
+    static long crnt = 17;
+    
+    static long rand()
+    {
+    	crnt *= 349820432;
+    	crnt += 543857345;
+    	crnt %= 1000000007;
+    	return crnt % 360;
+    }
+    
     /**
      * Returns a random Direction
      * @return a random Direction
      */
     static Direction randomDirection() {
-        return new Direction((float)Math.random() * 2 * (float)Math.PI);
+        return new Direction((float) (rand() / Math.PI / 2));
     }
 
     /**
@@ -248,6 +338,7 @@ public strictfp class RobotPlayer {
     	case GARDENER: return CHANNEL_NUMBER_OF_GARDENERS;
     	case SOLDIER: return CHANNEL_NUMBER_OF_SOLDIERS;
     	case LUMBERJACK: return CHANNEL_NUMBER_OF_LUMBERJACKS;
+    	case SCOUT: return CHANNEL_NUMBER_OF_SCOUTS;
     	}
     	throw new RuntimeException();
     }
@@ -283,5 +374,17 @@ public strictfp class RobotPlayer {
         float perpendicularDist = (float)Math.abs(distToRobot * Math.sin(theta)); // soh cah toa :)
 
         return (perpendicularDist <= rc.getType().bodyRadius);
+    }
+    
+    public static MapLocation readPoint(int pos) throws GameActionException
+    {
+    	return new MapLocation(rc.readBroadcast(pos), rc.readBroadcast(pos + 1));
+    }
+    
+    public static int writePoint(int pos, MapLocation val) throws GameActionException
+    {
+    	rc.broadcast(pos++, (int) val.x);
+    	rc.broadcast(pos++, (int) val.y);
+    	return pos;
     }
 }
